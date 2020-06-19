66c615bb05b075796c9d1958cb6f4021e2b56e6e
Niranjan <vaddepalli.niranjan@gmail.com>
2020-06-19 18:18:11 +0530

    Initial Commit

---
+++ iOS_Exercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_Exercise
+//
+//  Created by Niranjan on 19/06/20.
+//  Copyright © 2020 Niranjan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
